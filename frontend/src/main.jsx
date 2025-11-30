97fa36d04ae6306a52d54f75bbcf2e0413964f6c
MOHAMED HALIM ESSAMGUI <157497740+modev7@users.noreply.github.com>
2025-12-01 00:09:10 +0100

    main.jsx
     CSS.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-// 👇 CRITICAL FIX: ADD THIS LINE 👇
+
 import "react-toastify/dist/ReactToastify.css"; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
